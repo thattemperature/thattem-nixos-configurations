1d64ef8ffd9c3dbe7856f0ac4699c662f1099be4
thattemperature <2719023332@qq.com>
2026-04-28 23:59:31 +0800

    Fix syncthing service name space issue.

--- services/syncthing.nix
+++ services/syncthing.nix
@@ -21,45 +21,45 @@
 
       (lib.mkIf config.thattem.nixos.special.enable {
         services.syncthing = {
-          key = config.private.syncthing.key;
-          cert = config.private.syncthing.cert;
+          key = config.thattem.private.syncthing.key;
+          cert = config.thattem.private.syncthing.cert;
           settings = {
-            devices = config.private.syncthing.devices;
+            devices = config.thattem.private.syncthing.devices;
             folders = {
               "default" = {
                 label = "Default Folder";
                 id = "default";
                 path = "~/Sync/Default";
                 type = "receiveonly";
-                devices = config.private.syncthing.default-folder-devices;
+                devices = config.thattem.private.syncthing.default-folder-devices;
               };
               "temporary" = {
                 label = "Temporary Files";
                 id = "temporary";
                 path = "~/Sync/Temporary";
                 type = "receiveonly";
-                devices = config.private.syncthing.temporary-folder-devices;
+                devices = config.thattem.private.syncthing.temporary-folder-devices;
               };
               "archive" = {
                 label = "Archived Files";
                 id = "archive";
                 path = "~/Sync/Archive";
                 type = "receiveonly";
-                devices = config.private.syncthing.archive-folder-devices;
+                devices = config.thattem.private.syncthing.archive-folder-devices;
               };
               "secret" = {
                 label = "Secret Files";
                 id = "secret";
                 path = "~/Sync/Secret";
                 type = "receiveencrypted";
-                devices = config.private.syncthing.secret-folder-devices;
+                devices = config.thattem.private.syncthing.secret-folder-devices;
               };
               "secret-2" = {
                 label = "Second Secret Files";
                 id = "secret-2";
                 path = "~/Sync/Secret-2";
                 type = "receiveencrypted";
-                devices = config.private.syncthing.secret-folder-2-devices;
+                devices = config.thattem.private.syncthing.secret-folder-2-devices;
               };
             };
             options = {
@@ -72,7 +72,7 @@
               user = "thattemperature";
             };
           };
-          guiPasswordFile = config.age.secrets.syncthing-password.path;
+          guiPasswordFile = config.thattem.secrets.passwords.syncthing.path;
         };
       })
 
