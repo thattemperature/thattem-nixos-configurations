d9b5c751b4855b4044f4d3465ea8ebcd9b436ecf
thattemperature <2719023332@qq.com>
2026-08-06 12:05:02 +0800

    Change clash-verge systemd settings.

--- services/systemd.nix
+++ services/systemd.nix
@@ -4,29 +4,18 @@
 
   config = lib.mkMerge [
 
-    # (lib.mkIf config.programs.clash-verge.enable (
-    #   lib.mkIf config.programs.clash-verge.serviceMode {
-    #     systemd.services.clash-verge = lib.mkMerge [
+    (lib.mkIf config.programs.clash-verge.enable (
+      lib.mkIf config.programs.clash-verge.serviceMode {
+        systemd.services.clash-verge = lib.mkMerge [
 
-    #       {
-    #         after = [
-    #           "network-online.target"
-    #           "NetworkManager-wait-online.service"
-    #         ];
-    #         wants = [
-    #           "network-online.target"
-    #           "NetworkManager-wait-online.service"
-    #         ];
-    #       }
+          (lib.mkIf config.services.tailscale.enable {
+            after = [ "tailscaled.service" ];
+            wants = [ "tailscaled.service" ];
+          })
 
-    #       (lib.mkIf config.services.tailscale.enable {
-    #         after = [ "tailscaled.service" ];
-    #         wants = [ "tailscaled.service" ];
-    #       })
-
-    #     ];
-    #   }
-    # ))
+        ];
+      }
+    ))
 
   ];
 
